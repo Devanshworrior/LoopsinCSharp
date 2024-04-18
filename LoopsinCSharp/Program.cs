a03dbdd0a0b5424e5180ab55cf6a876067a1a10b
Devanshworrior <devanshjangid269@gmail.com>
2024-04-18 20:31:32 +0530

    first commit

---
+++ LoopsinCSharp/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoopsinCSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1. While Loop
+            int i = 0;
+            while (i < 300)
+            {
+                Console.WriteLine(i+1);
+                i++;
+            }
+
+            //2. Do While Loop
+            int k = 0;
+            do
+            {
+                Console.WriteLine(k+1);
+                k++;
+            } while (k > 500);
+
+            //3. For Loop
+            ;
+            for(int q=0; q<500; q++)
+            {
+                Console.WriteLine(q+1);
+            }
+
+            //Break And Continue
+            for (int v = 0; v < 500; v++)
+            {
+                if (v == 0)
+                {
+                    continue;
+                }
+                Console.WriteLine(v + 1);
+                //break;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ex/ex/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int q=0; q<450; q++)
+            {
+                Console.WriteLine(q+1);
+            }
+            Console.ReadLine();
+        }
+    }
+}
